1deeb0b0976c2ebeb9922ca59cc0fd7220ae5645
soandrade6 <sofia.andradep06@gmail.com>
2024-05-19 20:56:53 -0500

    Updated projects routes

--- src/routes/projects.routes.js
+++ src/routes/projects.routes.js
@@ -12,9 +12,9 @@ const router = Router();
 
 router.get("/projects", getProjects);
 router.post("/projects", createProject);
-router.put("/projects/:project_id", updateProject);
-router.delete("/projects/:project_id", deleteProject);
-router.get("/projects/:project_id", getProject); //To obtain an especific project
-router.get("/:project_id/tasks", getProjectTasks);
+router.put("/projects/:id", updateProject);
+router.delete("/projects/:id", deleteProject);
+router.get("/projects/:id", getProject); //To obtain an especific project
+router.get("/:id/tasks", getProjectTasks);
 
 export default router;
